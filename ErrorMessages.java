02428662e2a06ff974106b92e479c8bf65e99a1e
Jay Evans <Jay@rescomp-14-265467.stanford.edu>
2015-11-11 23:43:24 -0800

    Framework for error checking started

--- ErrorMessages.java
+++ ErrorMessages.java
@@ -1,11 +1,34 @@
-import java.uitl.*;
+import java.util.*;
 
 public class ErrorMessages {
-    
-    public Map<String, List<String>> errors;
+   
+    /* error messages */
+    public static final String USERNAME_UNAVAILABLE = 
+        "That username is unavailable. Please try another.";
+    public static final String USERNAME_EMPTY = 
+        "Please enter a username.";
+    public static final String USERNAME_INVALID = 
+        "Please limit your username to only characters and numbers.";
+    public static final String PASSWORD_INSUFFICIENT = 
+        "Please choose a password at least 8 characters in length.";
+    public static final String PASSWORD_EMPTY = 
+        "Please provide a password at least 8 characters long";
+    public static final String PASSWORD_MATCH_FAILED =
+        "The two password entries don't match. Please try again.";
+
+    public Map<String, ArrayList<String>> errors;
 
     public ErrorMessages() {
-        this.errors = new HashMap<String, <ArrayList<String>>();
+        this.errors = new HashMap<String, ArrayList<String>>();
     }
+    
+    public void addError(String uid, String message) {
+        ArrayList<String> errorMessages = this.errors.get(uid);
+        if(errorMessages == null)
+            errorMessages = new ArrayList<String>();
+        errorMessages.add(message);
 
+        /* TODO: do I need to put it again? or will it work without? */
+        this.errors.put(uid, errorMessages);
+    }   
 }
