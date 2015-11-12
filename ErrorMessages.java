d3032192b604ea781c5326f9af4ce2b54dbccf21
Jay Evans <Jay@DN0a226105.SUNet>
2015-11-11 16:28:53 -0800

    beginning of User code :)

---
+++ ErrorMessages.java
@@ -0,0 +1,11 @@
+import java.uitl.*;
+
+public class ErrorMessages {
+    
+    public Map<String, List<String>> errors;
+
+    public ErrorMessages() {
+        this.errors = new HashMap<String, <ArrayList<String>>();
+    }
+
+}
