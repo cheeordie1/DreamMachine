d997788bfd3a347512f0631b05b4d98d21ef5fde
Nicholas Chee-Awai <cheeordie1@gmail.com>
2015-11-15 22:09:11 -0800

    actually added the new stuff from last commit

---
+++ DreamMachine/src/assignment/LogoutServlet.java
@@ -0,0 +1,37 @@
+package assignment;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet(description = "Servlet to handle logging out", urlPatterns = { "/logout" })
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getSession().getAttribute("loggedIn").toString().equals("true")) {
+			request.getSession().setAttribute("loggedIn", "false");
+			request.getSession().removeAttribute("username");
+			request.getSession().removeAttribute("uid");
+		}
+		response.sendRedirect("http://localhost:8080/DreamMachine/home");
+	}
+
+}
