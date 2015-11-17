2c417ce3e333fd977e17420b88c0b4fa6a576a3d
Nicholas Chee-Awai <cheeordie1@gmail.com>
2015-11-16 16:49:27 -0800

    added new files. Don't dev on stage :(

---
+++ DreamMachine/src/assignment/Friend.java
@@ -0,0 +1,21 @@
+package assignment;
+
+import java.util.List;
+
+public class Friend {
+	/* instance variables */
+	
+	/* database variables */
+	int user_id1;
+	int user_id2;
+	
+	/**
+	 * Search the database friend
+	 * @param user_id The id of the user to find friends of
+	 * @return a list of all friends of the user associated with the user_id
+	 */
+	public static List<Friend> searchByUserId(int user_id) {
+		return null;
+	}
+	
+}
