aedd162cb46b091384a9d5a847e59c1075f86fd8
Jay Evans <Jay@rescomp-14-265467.stanford.edu>
2015-12-04 17:00:42 -0800

    goop before pushing to stage

--- DreamMachine/src/assignment/BlockServlet.java
+++ DreamMachine/src/assignment/BlockServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BlockServlet
  */
-@WebServlet("/BlockServlet")
+@WebServlet(urlPatterns = {"/block"})
 public class BlockServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
