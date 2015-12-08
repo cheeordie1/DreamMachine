35f89dc9cf64105fad6be14034145bf09e4f2365
morhm <morozco2@stanford.edu>
2015-12-08 08:07:01 -0800

    Update DBInfo.java

--- DreamMachine/src/assignment/DBInfo.java
+++ DreamMachine/src/assignment/DBInfo.java
@@ -1,8 +1,8 @@
 package assignment;
 
 public class DBInfo {
-	public static final String MYSQL_USERNAME = "";
-	public static final String MYSQL_PASSWORD = "";
-	public static final String MYSQL_DATABASE_SERVER = "";
-	public static final String MYSQL_DATABASE_NAME = "";
+	public static final String MYSQL_USERNAME = "ccs108morozco2";
+	public static final String MYSQL_PASSWORD = "3NV9HqrSZv2KxhdX";
+	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
+	public static final String MYSQL_DATABASE_NAME = "c_cs108_morozco2";
 }
