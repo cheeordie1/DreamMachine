7cd4053d454b2d2f23391450474b3a8bacb63cac
morhm <morozco2@stanford.edu>
2015-12-08 08:00:40 -0800

    Create DBInfo.java

---
+++ DreamMachine/src/assignment/DBInfo.java
@@ -0,0 +1,8 @@
+package assignment;
+
+public class DBInfo {
+	public static final String MYSQL_USERNAME = "";
+	public static final String MYSQL_PASSWORD = "";
+	public static final String MYSQL_DATABASE_SERVER = "";
+	public static final String MYSQL_DATABASE_NAME = "";
+}
